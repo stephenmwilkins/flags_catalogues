63a7143e790335088e82bff7c748c5ad10c48e9e
jturner200 <jack.turner200@gmail.com>
2023-04-04 13:18:49 +0100

    written into functions

--- create_HDF5_catalogues/update_spurious.py
+++ create_HDF5_catalogues/update_spurious.py
@@ -2,7 +2,9 @@ import numpy as np
 import h5py
 
 def update_spurious(survey, version, pointing):
-
+    '''
+    Create or update a dataset indicating which objects have been identified as spurious.
+    '''
     survey = survey.upper()
     survey_dir = f'/Users/jt458/{survey.lower()}'
 
@@ -13,6 +15,7 @@ def update_spurious(survey, version, pointing):
 
     catalogue_filename = f'{catalogue_id}.h5'
 
+    # Load in external file with list of spurious objects. Text file with each spurious id on a new line.
     spurious_list = list(np.loadtxt(f'{catalogue_id}-spurious.dat', dtype=int))
 
     with h5py.File(catalogue_filename, 'a') as hf:
@@ -21,6 +24,6 @@ def update_spurious(survey, version, pointing):
 
         if 'spurious' in hf.keys():
             del hf['spurious']
-
+        # True if spurious.
         hf['spurious'] = np.zeros(len(hf['photom/ID'][:]))
         hf['spurious'][spurious_ids] = 1
